1b8c527d3741db689b62e00a61c3d823e9ae0fe6
Natalia <nataliapanchenko@ukr.net>
2025-12-28 21:56:19 +0100

    edit solutions js/create-element

--- js/js-create-element/js-createelement_social-media-post/js/index.js
+++ js/js-create-element/js-createelement_social-media-post/js/index.js
@@ -9,22 +9,26 @@ const likeButton = document.querySelector('[data-js="like-button"]');
 likeButton.addEventListener("click", handleLikeButtonClick);
 
 const post = document.createElement("article");
-post.classList.add("post");
-const p = document.createElement("p");
-p.classList.add("post__content");
-p.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+const content = document.createElement("p");
 const footer = document.createElement("footer");
-footer.classList.add("post__footer");
 const span = document.createElement("span");
+const button = document.createElement("button");
+
+post.classList.add("post");
+content.classList.add("post__content");
+content.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+footer.classList.add("post__footer");
 span.classList.add("post__username");
 span.textContent = "@username";
-const button = document.createElement("button");
 button.classList.add("post__button");
 button.type = "button";
 button.setAttribute("data-js", "like-button");
 button.textContent = "♥ Like";
 
-post.append(p, footer);
+button.addEventListener("click", handleLikeButtonClick);
+
+post.append(content, footer);
 footer.append(span, button);
 
 document.body.append(post);
