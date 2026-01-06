b3801d83322438ed5310c734650188af774eece6
Natalia <nataliapanchenko@ukr.net>
2026-01-06 12:38:56 +0100

    add challenges for-console, creating a list, nav-bar, while

---
+++ js/js-loops/js-loops_forin-select-nav/js/dist/index.dev.js
@@ -0,0 +1,56 @@
+"use strict";
+
+console.clear();
+var main = document.querySelector('[data-js="main"]'); // Part 1: Creating a Select Input
+
+var languages = {
+  DE: "German",
+  EN: "English",
+  ES: "Spanish",
+  FR: "French",
+  IT: "Italian",
+  NL: "Dutch",
+  PT: "Portuguese",
+  RU: "Russian",
+  ZH: "Chinese"
+};
+var select = document.createElement("select");
+select.name = "languages";
+main.append(select); // --v-- write or modify code below this line --v--
+
+for (var language in languages) {
+  var option = document.createElement("option");
+  option.textContent = languages[language];
+  option.value = languages[language];
+  select.append(option);
+} // --^-- write or modify code above this line --^--
+// Part 2: Creating a Navigation Bar
+
+
+var nav = {
+  home: {
+    href: "/home",
+    text: "Home"
+  },
+  about: {
+    href: "/about",
+    text: "About"
+  },
+  contact: {
+    href: "/contact",
+    text: "Contact"
+  }
+};
+var navElement = document.createElement("nav");
+var ul = document.createElement("ul");
+main.append(navElement);
+navElement.append(ul); // --v-- write or modify code below this line --v--
+
+for (var link in nav) {
+  var li = document.createElement("li");
+  var a = document.createElement("a");
+  a.href = nav[link].href;
+  a.textContent = nav[link].text;
+  li.append(a);
+  ul.append(li);
+} // --^-- write or modify code above this line --^--
