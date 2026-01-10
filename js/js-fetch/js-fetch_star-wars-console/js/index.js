d506271865ca48461fa8f9abf66db8849ff64bc1
Natalia <nataliapanchenko@ukr.net>
2026-01-10 20:19:13 +0100

    add features

--- js/js-fetch/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -11,12 +11,9 @@ async function fetchData() {
   console.log(data.results[0].name);
   console.log(data.results[4].birth_year);
   console.log(data.results[9].homeworld);
-  console.log(
-    "The eye color of " +
-      data.results[2].name +
-      " is " +
-      data.results[2].eye_color
-  );
+
+  const r2d2 = data.results.find((person) => person.name === "R2-D2");
+  console.log("R2-D2s eye color:", r2d2.eye_color);
 }
 
 fetchData();
