0ce18e6afc3d75d28bc2c36de2dd1c0b700f2696
Natalia <nataliapanchenko@ukr.net>
2026-01-15 16:43:37 +0100

    add hello world article challenge

--- react/react-basics_attributes/src/App.jsx
+++ react/react-basics_attributes/src/App.jsx
@@ -1,5 +1,21 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article();
+}
+
+export function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Title</h2>
+      <label htmlFor="newInput"></label>
+      <input type="text" id="newInput" />
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/The_Beatles"
+      >
+        The Beatles
+      </a>
+    </article>
+  );
 }
