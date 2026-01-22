e9275fee83d563af0ed0b3ae86f84b253dd6031f
Natalia <nataliapanchenko@ukr.net>
2026-01-22 13:00:48 +0100

    edit feature Pokémon API with Paging

--- react/react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
+++ react/react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,22 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setCoords({
+        longitude: data.longitude,
+        latitude: data.latitude,
+      });
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
+  useEffect(() => {
+    getISSCoords();
+  }, []);
 
   return (
     <main>
--- react/react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react/react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -1,43 +1,40 @@
 import { useEffect, useState } from "react";
 
+const PAGE_SIZE = 20;
+
 export default function PokemonList() {
-  const [data, setData] = useState();
-  const [currentPage, setCurrentPage] = useState(0);
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon?offset=${currentPage}`,
+          `https://pokeapi.co/api/v2/pokemon?offset=${page * PAGE_SIZE}`,
         );
         const data = await response.json();
-        setData(data);
+        setPokemon(data.results);
       } catch (error) {
         console.log(error);
       }
     }
-
     loadPokemon();
-  }, [currentPage]);
+  }, [page]);
 
   return (
     <main>
       <button
         type="button"
-        onClick={() => setCurrentPage((prev) => prev - 1)}
-        disabled={!data?.previous}
+        onClick={() => setPage((prev) => prev - 1)}
+        disabled={page === 0}
       >
         Previous Page
       </button>
-      <button
-        type="button"
-        onClick={() => setCurrentPage((prev) => prev + 1)}
-        disabled={!data?.next}
-      >
+      <button type="button" onClick={() => setPage((prev) => prev + 1)}>
         Next Page
       </button>
       <ul>
-        {data?.results.map(({ name }) => (
+        {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
