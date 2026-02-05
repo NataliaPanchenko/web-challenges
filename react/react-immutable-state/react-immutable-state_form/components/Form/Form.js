e74738f6157a65f3363a5c8948cb425c4e26aed1
Natalia <nataliapanchenko@ukr.net>
2026-02-05 15:52:00 +0100

    add challenge Form

---
+++ react/react-immutable-state/react-immutable-state_form/components/Form/Form.js
@@ -0,0 +1,63 @@
+import { useImmer } from "use-immer";
+import { StyledForm, StyledInputContainer } from "./Form.styled";
+
+export default function Form() {
+  const [mountain, updateMountain] = useImmer({
+    name: "Mount Everest",
+    values: {
+      altitude: 8848,
+      mountainRange: "Himalayas",
+    },
+  });
+
+  function handleNameChange(event) {
+    updateMountain((prev) => {
+      prev.name = event.target.value;
+    });
+  }
+
+  function handleAltitudeChange(event) {
+    updateMountain((prev) => {
+      prev.values.altitude = event.target.value;
+    });
+  }
+
+  function handleMountainRangeChange(event) {
+    updateMountain((prev) => {
+      prev.values.mountainRange = event.target.value;
+    });
+  }
+
+  return (
+    <StyledForm>
+      <StyledInputContainer>
+        <label htmlFor="name">Name:</label>
+        <input id="name" value={mountain.name} onChange={handleNameChange} />
+      </StyledInputContainer>
+      <StyledInputContainer>
+        <label htmlFor="altitude">Altitude:</label>
+        <input
+          id="altitude"
+          value={mountain.values.altitude}
+          onChange={handleAltitudeChange}
+        />
+      </StyledInputContainer>
+      <StyledInputContainer>
+        <label htmlFor="mountainRange">Mountain Range:</label>
+        <input
+          id="mountainRange"
+          value={mountain.values.mountainRange}
+          onChange={handleMountainRangeChange}
+        />
+      </StyledInputContainer>
+      <output>
+        <i>{mountain.name}</i>
+        {" is "}
+        {mountain.values.altitude}
+        {" meters high"}
+        <br />
+        (and located in the {mountain.values.mountainRange})
+      </output>
+    </StyledForm>
+  );
+}
