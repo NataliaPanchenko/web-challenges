94e6e21c4f30466702aa9cee6dc1157620e9f86e
Natalia <nataliapanchenko@ukr.net>
2026-02-12 15:55:36 +0100

    add challenge chance

--- backend/backend-basics/backend-basics_chance/index.js
+++ backend/backend-basics/backend-basics_chance/index.js
@@ -1,3 +1,5 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+server.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
