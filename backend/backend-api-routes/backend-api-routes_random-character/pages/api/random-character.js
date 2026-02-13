db0f39a4f631eb89dc429f3af0162f3d3469eb98
Natalia <nataliapanchenko@ukr.net>
2026-02-13 16:11:53 +0100

    add challenge Random Character

--- backend/backend-api-routes/backend-api-routes_random-character/pages/api/random-character.js
+++ backend/backend-api-routes/backend-api-routes_random-character/pages/api/random-character.js
@@ -2,14 +2,19 @@ import Chance from "chance";
 
 const chance = new Chance();
 
-export default function handler(request, response) {
+export default function handler(req, res) {
   const character = {
     firstName: chance.first(),
     lastName: chance.last(),
     age: chance.age(),
+    birthday: chance.birthday(),
     profession: chance.profession(),
     adress: chance.address(),
+    phone: chance.phone(),
+    twitter: chance.twitter(),
+    geohash: chance.geohash(),
+    animal: chance.animal(),
   };
 
-  response.status(200).json(character);
+  res.status(200).json(character);
 }
